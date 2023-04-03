b04811a499bcea7f41c297318a83fd73f885e314
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-04-03 18:50:35 +0800

    refactor: tidy up grammar definition in single line form

--- src/ch3/let-lang/grammar.rkt
+++ src/ch3/let-lang/grammar.rkt
@@ -4,62 +4,33 @@
 (provide (all-defined-out))
 
 (define the-grammar
-  '(
-    (program (expression) a-program)
-
+  '((program (expression) a-program)
     (expression (number) const-exp)
-    (expression
-     ("-" "(" expression "," expression ")")
-     diff-exp)
-
-    (expression
-     ("+" "(" expression "," expression ")")
-     sum-exp)
-
-    (expression
-     ("*" "(" expression "," expression ")")
-     mul-exp)
-
-    (expression
-     ("/" "(" expression "," expression ")")
-     div-exp)
-
-    (expression
-     ("equal?" "(" expression "," expression ")")
-     equal?-exp)
-
-    (expression
-     ("greater?" "(" expression "," expression ")")
-     greater?-exp)
-
-    (expression
-     ("less?" "(" expression "," expression ")")
-     less?-exp)
-
-    (expression
-     ("minus" "(" expression  ")")
-     minus-exp)
-
-    (expression
-     ("zero?" "(" expression ")")
-     zero?-exp)
-
-    (expression
-     ("if" expression "then" expression "else" expression)
-     if-exp)
-
     (expression (identifier) var-exp)
+    (expression ("let" identifier "=" expression "in" expression) let-exp)
 
+    ; arithmetic
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("+" "(" expression "," expression ")") sum-exp)
+    (expression ("*" "(" expression "," expression ")") mul-exp)
+    (expression ("/" "(" expression "," expression ")") div-exp)
+    (expression ("minus" "(" expression  ")") minus-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+
+    ; comparision
+    (expression ("equal?" "(" expression "," expression ")") equal?-exp)
+    (expression ("greater?" "(" expression "," expression ")") greater?-exp)
+    (expression ("less?" "(" expression "," expression ")") less?-exp)
+
+    ; list
     (expression ("cons" "(" expression "," expression ")") cons-exp)
     (expression ("car" "(" expression ")") car-exp)
     (expression ("cdr" "(" expression ")") cdr-exp)
     (expression ("emptylist") emptylist-exp)
-
     (expression ("null?" "(" expression ")") null?-exp)
-
     (expression ("list" "(" expression (arbno "," expression)")") list-exp)
 
-    (expression
-     ("let" identifier "=" expression "in" expression)
-     let-exp)
-    ))
+    ; control
+    (expression ("if" expression "then" expression "else" expression) if-exp)
+    )
+  )
