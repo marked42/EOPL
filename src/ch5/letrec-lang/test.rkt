ae08cded2335f43d44f1a9cffd73667aca952e1a
penghui <penghui.zhang@foxmail.com>
2023-04-13 01:14:02 +0800

    feat(cps): support more expression types
    1. let-exp with multiple variable declarations
    2. proc-exp with arbitrary number of arguments
    3. call expression
    4. letrec expression supporting recursive call

--- src/ch5/letrec-lang/test.rkt
+++ src/ch5/letrec-lang/test.rkt
@@ -34,36 +34,36 @@
 (equal-answer? (run "i") 1 "built in var i is 1")
 (equal-answer? (run "v") 5 "built in var i is 5")
 (equal-answer? (run "x") 10 "built in var i is 10")
-; (equal-answer? (run "let a = 1 in -(a, x)") -9 "let exp")
+(equal-answer? (run "let a = 1 x = 2 in -(a, x)") -1 "let exp")
 
 ; ; exer 3.16
-; (equal-answer? (run "let x = 30 in let x = -(x,1) y = -(x,2) in -(x,y)") 1 "let-exp with arbitrary number of vars")
+(equal-answer? (run "let x = 30 in let x = -(x,1) y = -(x,2) in -(x,y)") 1 "let-exp with arbitrary number of vars")
 
-; (equal-answer? (run "let f = proc (x) -(x,11) in (f (f 77))") 55 "proc-exp")
-; ; IIFE
-; (equal-answer? (run "(proc (f) (f (f 77)) proc (x) -(x,11))") 55 "proc-exp")
+(equal-answer? (run "let f = proc (x) -(x,11) in (f (f 77))") 55 "proc-exp")
+; IIFE
+(equal-answer? (run "(proc (f) (f (f 77)) proc (x) -(x,11))") 55 "proc-exp")
 
-; ; exer 3.20
-; (equal-answer? (run "let f = proc(x) proc(y) -(x,-(0,y)) in ((f 3) 4)") 7 "letproc-exp")
+; exer 3.20
+(equal-answer? (run "let f = proc(x) proc(y) -(x,-(0,y)) in ((f 3) 4)") 7 "letproc-exp")
 
-; (equal-answer? (run "let f = proc(x, y) -(x,-(0,y)) in (f 3 4)") 7 "letproc-exp")
+(equal-answer? (run "let f = proc(x, y) -(x,-(0,y)) in (f 3 4)") 7 "letproc-exp")
 
-; (equal-answer? (run "
-; letrec double(x)
-;   = if zero?(x) then 0 else -((double -(x,1)), -2)
-;     in (double 6)
-; ") 12 "letrec-exp")
+(equal-answer? (run "
+letrec double(x)
+  = if zero?(x) then 0 else -((double -(x,1)), -2)
+    in (double 6)
+") 12 "letrec-exp")
 
-; (equal-answer? (run "
-; letrec sum(x, y)
-;   = if zero?(x) then y else -((sum -(x,1) y), -1)
-;     in (sum 3 4)
-; ") 7 "letrec-exp with multiple arguments")
+(equal-answer? (run "
+letrec sum(x, y)
+  = if zero?(x) then y else -((sum -(x,1) y), -1)
+    in (sum 3 4)
+") 7 "letrec-exp with multiple arguments")
 
-; ; (odd 13) -> (even 12) -> (odd 11) -> ... -> (even 0) -> 1
-; (equal-answer? (run "
-; letrec
-; even(x) = if zero?(x) then 1 else (odd -(x,1))
-; odd(x) = if zero?(x) then 0 else (even -(x,1))
-; in (odd 13)
-; ") 1 "letrec-exp with multiple procedures")
+; (odd 13) -> (even 12) -> (odd 11) -> ... -> (even 0) -> 1
+(equal-answer? (run "
+letrec
+even(x) = if zero?(x) then 1 else (odd -(x,1))
+odd(x) = if zero?(x) then 0 else (even -(x,1))
+in (odd 13)
+") 1 "letrec-exp with multiple procedures")
