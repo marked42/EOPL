0f175bfd76f51ee4bd11ddf88e65da71d8cd6913
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-04-19 18:58:31 +0800

    feat(cps-lambda): proc expression and call expression

--- src/ch5/continuation-as-lambda/test.rkt
+++ src/ch5/continuation-as-lambda/test.rkt
@@ -40,13 +40,13 @@
 (equal-answer? (run "let x = 30 in let x = -(x,1) y = -(x,2) in -(x,y)") 1 "let-exp with arbitrary number of vars")
 
 (equal-answer? (run "let f = proc (x) -(x,11) in (f (f 77))") 55 "proc-exp")
-; ; IIFE
-; (equal-answer? (run "(proc (f) (f (f 77)) proc (x) -(x,11))") 55 "proc-exp")
+; IIFE
+(equal-answer? (run "(proc (f) (f (f 77)) proc (x) -(x,11))") 55 "proc-exp")
 
-; ; exer 3.20
-; (equal-answer? (run "let f = proc(x) proc(y) -(x,-(0,y)) in ((f 3) 4)") 7 "letproc-exp")
+; exer 3.20
+(equal-answer? (run "let f = proc(x) proc(y) -(x,-(0,y)) in ((f 3) 4)") 7 "letproc-exp")
 
-; (equal-answer? (run "let f = proc(x, y) -(x,-(0,y)) in (f 3 4)") 7 "letproc-exp")
+(equal-answer? (run "let f = proc(x, y) -(x,-(0,y)) in (f 3 4)") 7 "letproc-exp")
 
 ; (equal-answer? (run "
 ; letrec double(x)
