6a2fb7e30a27af36d2af13ba4198faed56584832
penghui <penghui.zhang@foxmail.com>
2023-04-20 23:56:19 +0800

    refactor(cps-ds): rename cont name to *-cont
    cont variant name corresponds to expression, for expression of multiple steps
    when evaluating, only first step cont is directly used in interpreter.rkt,
    helper cont variants are hidden inside continuation.rkt as implementation details.

--- src/ch5/letrec-lang/interpreter.rkt
+++ src/ch5/letrec-lang/interpreter.rkt
@@ -19,18 +19,18 @@
  ["continuation.rkt" (
                       end-cont
                       apply-cont
-                      diff-cont-1
+                      diff-cont
                       zero?-cont
                       if-cont
                       exps-cont
                       let-cont
-                      call-exp-cont
-                      cons-exp-cont-1
-                      null?-exp-cont
-                      car-exp-cont
-                      cdr-exp-cont
-                      list-exp-cont
-                      begin-exp-cont
+                      call-cont
+                      cons-cont
+                      null?-cont
+                      car-cont
+                      cdr-cont
+                      list-cont
+                      begin-cont
                       set-rhs-cont
                       )]
  ["call.rkt" (eval-call-by-value-operand)]
@@ -53,7 +53,7 @@
   (cases expression exp
     (const-exp (num) (apply-cont cont (num-val num)))
     (diff-exp (exp1 exp2)
-              (value-of/k exp1 env (diff-cont-1 cont exp2 env))
+              (value-of/k exp1 env (diff-cont cont exp2 env))
               )
     (zero?-exp (exp1)
                (value-of/k exp1 env (zero?-cont cont))
@@ -71,7 +71,7 @@
               (apply-cont cont (proc-val (procedure (cons first-var rest-vars) body env)))
               )
     (call-exp (rator rands)
-              (value-of/k rator env (call-exp-cont cont rands env))
+              (value-of/k rator env (call-cont cont rands env))
               )
     (letrec-exp (p-names b-vars-list p-bodies body)
                 (let ((new-env (build-circular-extend-env-rec-mul-vec p-names b-vars-list p-bodies env)))
@@ -81,22 +81,22 @@
     ; list
     (emptylist-exp () (apply-cont cont (null-val)))
     (cons-exp (exp1 exp2)
-              (value-of/k exp1 env (cons-exp-cont-1 cont exp2 env))
+              (value-of/k exp1 env (cons-cont cont exp2 env))
               )
     (null?-exp (exp1)
-               (value-of/k exp1 env (null?-exp-cont cont))
+               (value-of/k exp1 env (null?-cont cont))
                )
     (car-exp (exp1)
-             (value-of/k exp1 env (car-exp-cont cont))
+             (value-of/k exp1 env (car-cont cont))
              )
     (cdr-exp (exp1)
-             (value-of/k exp1 env (cdr-exp-cont cont))
+             (value-of/k exp1 env (cdr-cont cont))
              )
     (list-exp (exp1 exps)
-              (value-of-exps/k (cons exp1 exps) '() env (list-exp-cont cont) eval-call-by-value-operand)
+              (value-of-exps/k (cons exp1 exps) '() env (list-cont cont) eval-call-by-value-operand)
               )
     (begin-exp (exp1 exps)
-               (value-of-exps/k (cons exp1 exps) '() env (begin-exp-cont cont) eval-call-by-value-operand)
+               (value-of-exps/k (cons exp1 exps) '() env (begin-cont cont) eval-call-by-value-operand)
                )
     (assign-exp (var exp1)
                 (value-of/k exp1 env (set-rhs-cont (apply-env env var) cont))
