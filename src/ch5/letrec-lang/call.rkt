609a1cc08352c2c45d9d1e4ec278ecc6706511a4
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-04-21 13:18:45 +0800

    refactor: extract shared utils call.rkt

--- src/ch5/letrec-lang/call.rkt
+++ src/ch5/letrec-lang/call.rkt
@@ -1,21 +1,14 @@
 #lang eopl
 
-(require
-  racket/lazy-require
-  ; use program and expression datatype
-  "../shared/expression.rkt"
-  )
-
+(require racket/lazy-require)
 (lazy-require
  ["interpreter.rkt" (value-of/k)]
- ["continuation.rkt" (apply-cont)]
- ["../shared/store.rkt" (deref)]
- ["../shared/environment.rkt" (apply-env)]
+ ["../shared/call.rkt" (make-eval-operand-call-by-value)]
  )
 
 (provide (all-defined-out))
 
 (define (eval-operand-call-by-value exp1 saved-env saved-cont)
   ; just get the value of exp1
-  (value-of/k exp1 saved-env saved-cont)
+  (make-eval-operand-call-by-value value-of/k exp1 saved-env saved-cont)
   )
--- src/ch5/shared/call.rkt
+++ src/ch5/shared/call.rkt
@@ -3,24 +3,21 @@
 (require
   racket/lazy-require
   ; use program and expression datatype
-  "../shared/expression.rkt"
+  "expression.rkt"
   )
 
 (lazy-require
- ["interpreter.rkt" (value-of/k)]
- ["continuation.rkt" (apply-cont)]
- ["../shared/store.rkt" (deref)]
- ["../shared/environment.rkt" (apply-env)]
+ ["environment.rkt" (apply-env)]
  )
 
 (provide (all-defined-out))
 
-(define (eval-operand-call-by-value exp1 saved-env saved-cont)
+(define (make-eval-operand-call-by-value value-of/k exp1 saved-env saved-cont)
   ; just get the value of exp1
   (value-of/k exp1 saved-env saved-cont)
   )
 
-(define (eval-operand-call-by-ref exp1 saved-env saved-cont)
+(define (make-eval-operand-call-by-ref value-of/k apply-cont exp1 saved-env saved-cont)
   (cases expression exp1
     (var-exp (var)
              ; call by ref
