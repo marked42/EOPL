912db87c6121eb58d592b7ba76d3892e6c82bded
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-04-23 18:04:34 +0800

    feat: start for registerized cps interpreter

---
+++ src/ch5/registerized/value.rkt
@@ -0,0 +1,36 @@
+#lang eopl
+
+(require racket/lazy-require "basic.rkt" "expression.rkt")
+(lazy-require
+ ["environment.rkt" (environment?)]
+ ["procedure.rkt" (proc?)]
+ )
+
+(provide (all-defined-out))
+
+(define-datatype expval expval?
+  (num-val (num number?))
+  (bool-val (bool boolean?))
+  (proc-val (proc1 proc?))
+  )
+
+(define (expval->num val)
+  (cases expval val
+    (num-val (num) num)
+    (else (report-expval-extractor-error 'num val))
+    )
+  )
+
+(define (expval->bool val)
+  (cases expval val
+    (bool-val (bool) bool)
+    (else (report-expval-extractor-error 'bool val))
+    )
+  )
+
+(define (expval->proc val)
+  (cases expval val
+    (proc-val (proc1) proc1)
+    (else (report-expval-extractor-error 'proc val))
+    )
+  )
