3030953b4d7bdbbc3e88429e4990304134d7a70c
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-04-26 10:19:10 +0800

    refactor: move exclusive test to their own module

--- src/ch5/exception-exer-5.37/test.rkt
+++ src/ch5/exception-exer-5.37/test.rkt
@@ -2,10 +2,18 @@
 
 (require racket/lazy-require)
 (lazy-require
- ["../shared/test.rkt" (run-tests run-test-exception run-test-wrong-number-of-args)]
+ ["../shared/test.rkt" (run-tests run-test-exception equal-answer?)]
  ["interpreter.rkt" (run)]
  )
 
-; (run-tests run)
-; (run-test-exception run)
+(define (run-test-wrong-number-of-args run)
+  (equal-answer? (run "
+  let f = proc (x) x
+    in try (f 1 2)
+       catch (m) 44
+  ") 44 "wrong number of args, f accepts only single parameter x, get (1, 2)")
+  )
+
+(run-tests run)
+(run-test-exception run)
 (run-test-wrong-number-of-args run)
--- src/ch5/exception-exer-5.38/test.rkt
+++ src/ch5/exception-exer-5.38/test.rkt
@@ -2,9 +2,14 @@
 
 (require racket/lazy-require)
 (lazy-require
- ["../shared/test.rkt" (run-tests run-test-exception run-test-division)]
+ ["../shared/test.rkt" (run-tests run-test-exception equal-answer?)]
  ["interpreter.rkt" (run)]
  )
+
+(define (run-test-division run)
+  (equal-answer? (run "try div(4, 2) catch (m) 44") 2 "4 divieded by 2 is 2")
+  (equal-answer? (run "try div(4, 0) catch (m) 44") 44 "throws error when divided by 0")
+  )
 
 (run-tests run)
 (run-test-exception run)
