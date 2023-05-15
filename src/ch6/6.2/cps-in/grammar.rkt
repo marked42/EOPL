85a09a1a1129515e8646cc4ac3a159806626c16b
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-05-15 10:35:46 +0800

    chore: CPS-in in chapter 6.2

---
+++ src/ch6/6.2/cps-in/grammar.rkt
@@ -0,0 +1,22 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(define the-grammar
+  '((program (expression) a-program)
+    (expression (number) const-exp)
+    (expression (identifier) var-exp)
+    (expression ("let" identifier "=" expression "in" expression) let-exp)
+    (expression ("letrec" (arbno identifier "(" (separated-list identifier ",") ")" "=" expression) "in" expression) letrec-exp)
+
+    ; arithmetic
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+
+    ; control
+    (expression ("if" expression "then" expression "else" expression) if-exp)
+
+    (expression ("proc" "(" identifier (arbno "," identifier) ")" expression) proc-exp)
+    (expression ("(" expression (arbno expression) ")") call-exp)
+    )
+  )
