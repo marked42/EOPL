857367b996315b6aa53fca5f327a938a3609c689
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-05-16 20:04:09 +0800

    feat: diff-exp/zero?-exp/var-exp

--- src/ch6/cps-lang/test.rkt
+++ src/ch6/cps-lang/test.rkt
@@ -20,14 +20,15 @@
                    sloppy-val)))))
 
 (equal-answer? (run "1") 1 "const exp")
-; (equal-answer? (run "-(1, 2)") -1 "diff exp")
-; (equal-answer? (run "zero? (0)") #t "zero? exp")
-; (equal-answer? (run "zero? (1)") #f "zero? exp")
+(equal-answer? (run "-(1, 2)") -1 "diff exp")
+(equal-answer? (run "zero? (0)") #t "zero? exp")
+(equal-answer? (run "zero? (1)") #f "zero? exp")
+(equal-answer? (run "i") 1 "built in var i is 1")
+(equal-answer? (run "v") 5 "built in var i is 5")
+(equal-answer? (run "x") 10 "built in var i is 10")
+
 ; (equal-answer? (run "if zero? (0) then 2 else 3") 2 "if exp")
 ; (equal-answer? (run "if zero? (1) then 2 else 3") 3 "if exp")
-; (equal-answer? (run "i") 1 "built in var i is 1")
-; (equal-answer? (run "v") 5 "built in var i is 5")
-; (equal-answer? (run "x") 10 "built in var i is 10")
 ; (equal-answer? (run "let a = 1 in -(a, x)") -9 "let exp")
 
 ; (equal-answer? (run "let f = proc (x) -(x,11) in (f (f 77))") 55 "proc-exp")
