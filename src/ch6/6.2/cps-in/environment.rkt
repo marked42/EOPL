a95155f4d8e6e95742336531e6666f39483aaaec
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-05-19 11:43:34 +0800

    feat: cps-in

---
+++ src/ch6/6.2/cps-in/environment.rkt
@@ -0,0 +1,67 @@
+#lang eopl
+
+(require racket/lazy-require racket/list "basic.rkt" "expression.rkt")
+(lazy-require
+ ["value.rkt" (num-val proc-val expval?)]
+ ["procedure.rkt" (procedure)]
+ )
+(provide (all-defined-out))
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (var identifier?)
+   (val expval?)
+   (env environment?)
+   )
+  (extend-env*
+   (vars (list-of identifier?))
+   (vals (list-of expval?))
+   (env environment?)
+   )
+  (extend-env-rec*
+   (p-names (list-of identifier?))
+   (b-varss (list-of (list-of identifier?)))
+   (p-bodies (list-of expression?))
+   (env environment?)
+   )
+  )
+
+(define (init-env)
+  (extend-env 'i (num-val 1)
+              (extend-env 'v (num-val 5)
+                          (extend-env 'x (num-val 10)
+                                      (empty-env)
+                                      )
+                          )
+              )
+  )
+
+(define (apply-env env search-var)
+  (cases environment env
+    (extend-env (var val saved-env)
+                (if (eqv? search-var var)
+                    val
+                    (apply-env saved-env search-var)
+                    )
+                )
+    (extend-env* (vars vals saved-env)
+                 (let ((index (index-of vars search-var)))
+                   (if index
+                       (list-ref vals index)
+                       (apply-env saved-env search-var)
+                       )
+                   )
+                 )
+    (extend-env-rec* (p-names b-varss p-bodies saved-env)
+                     (let ((index (index-of p-names search-var)))
+                       (if index
+                           ; use env as its parent env for recursive definition
+                           (proc-val (procedure (list-ref b-varss index) (list-ref p-bodies index) env))
+                           (apply-env saved-env search-var)
+                           )
+                       )
+                     )
+    (else (report-no-binding-found search-var))
+    )
+  )
---
+++ src/ch6/base/the-lexical-spec.rkt
@@ -0,0 +1,4 @@
+#lang eopl
+
+(require "../../base/the-lexical-spec.rkt")
+(provide (all-from-out  "../../base/the-lexical-spec.rkt"))
