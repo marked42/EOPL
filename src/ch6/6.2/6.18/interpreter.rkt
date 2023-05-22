c1b74b145d15222dcc4f0a41ac87513bab1ed883
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-05-22 19:46:20 +0800

    feat: exercise 6.18

--- src/ch6/6.2/6.18/interpreter.rkt
+++ src/ch6/6.2/6.18/interpreter.rkt
@@ -63,11 +63,11 @@
 
 (define (value-of-simple-exp exp1 env)
   (cases simple-expression exp1
-    (cps-const-exp (num) (num-val num))
-    (cps-var-exp (var) (apply-env env var))
+    (cps-const-exp (num) (value-of-const-or-var-exp exp1 env))
+    (cps-var-exp (var) (value-of-const-or-var-exp exp1 env))
     (cps-diff-exp (exp1 exp2)
-                  (let ((val1 (value-of-simple-exp exp1 env))
-                        (val2 (value-of-simple-exp exp2 env)))
+                  (let ((val1 (value-of-const-or-var-exp exp1 env))
+                        (val2 (value-of-const-or-var-exp exp2 env)))
                     (let ((num1 (expval->num val1))
                           (num2 (expval->num val2)))
                       (num-val (- num1 num2))
@@ -76,7 +76,7 @@
                   )
     ; true only if exp1 is number 0
     (cps-zero?-exp (exp1)
-                   (let ((val (value-of-simple-exp exp1 env)))
+                   (let ((val (value-of-const-or-var-exp exp1 env)))
                      (let ((num (expval->num val)))
                        (if (zero? num)
                            (bool-val #t)
@@ -92,7 +92,7 @@
                  (let ((nums (map
                               (lambda (exp)
                                 (expval->num
-                                 (value-of-simple-exp exp env)))
+                                 (value-of-const-or-var-exp exp env)))
                               exps)))
                    (num-val
                     (let sum-loop ((nums nums))
@@ -104,4 +104,12 @@
 
 (define (value-of-simple-exps exps env)
   (map (lambda (exp1) (value-of-simple-exp exp1 env)) exps)
+  )
+
+(define (value-of-const-or-var-exp exp1 env)
+  (cases simple-expression exp1
+    (cps-const-exp (num) (num-val num))
+    (cps-var-exp (var) (apply-env env var))
+    (else (eopl:error 'value-of-const-or-var-exp "unsupported simple-expression ~s " exp1))
+    )
   )
