2b58b44cec4ed3a7cb774452cd18837cbf04766b
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-05-22 19:52:24 +0800

    chore: start cps-lang on basis of cps-out lang

--- src/ch6/6.3/cps-lang/procedure.rkt
+++ src/ch6/6.3/cps-lang/procedure.rkt
@@ -1,9 +1,12 @@
 #lang eopl
 
-(require racket/lazy-require "basic.rkt" "environment.rkt")
+(require racket/lazy-require)
 (lazy-require
+ ["../../../base/basic.rkt" (identifier?)]
  ["expression.rkt" (tfexp?)]
- ["interpreter.rkt" (value-of/k)])
+ ["interpreter.rkt" (value-of/k)]
+ ["environment.rkt" (extend-env* environment?)]
+ )
 
 (provide (all-defined-out))
 
@@ -18,7 +21,11 @@
 (define (apply-procedure proc1 args cont)
   (cases proc proc1
     (procedure (vars body saved-env)
-               (value-of/k body (extend-mul-env vars args saved-env) cont)
+               (value-of/k body (extend-env* vars args saved-env) cont)
                )
     )
+  )
+
+(define (create-procedure vars args saved-env)
+  (procedure vars args saved-env)
   )
