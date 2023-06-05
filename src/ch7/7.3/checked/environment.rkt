b1217bfa0d3a8994ff2636c8bf12a83949c69a9a
penghui <penghui.zhang@foxmail.com>
2023-06-05 23:04:01 +0800

    feat: update extend-env to support multiple vars in environment

--- src/ch7/7.3/checked/environment.rkt
+++ src/ch7/7.3/checked/environment.rkt
@@ -11,8 +11,8 @@
 (define-datatype environment environment?
   (empty-env)
   (extend-env
-   (var identifier?)
-   (val expval?)
+   (vars (list-of identifier?))
+   (vals (list-of expval?))
    (env environment?)
    )
   (extend-env-rec
@@ -24,9 +24,9 @@
   )
 
 (define (init-env)
-  (extend-env 'i (num-val 1)
-              (extend-env 'v (num-val 5)
-                          (extend-env 'x (num-val 10)
+  (extend-env (list 'i) (list (num-val 1))
+              (extend-env (list 'v) (list (num-val 5))
+                          (extend-env (list 'x) (list (num-val 10))
                                       (empty-env)
                                       )
                           )
@@ -35,11 +35,20 @@
 
 (define (apply-env env search-var)
   (cases environment env
-    (extend-env (var val saved-env)
-                (if (eqv? search-var var)
-                    val
-                    (apply-env saved-env search-var)
-                    )
+    (extend-env (vars vals saved-env)
+                (letrec ((loop (lambda (vars vals saved-env)
+                                 (if (null? vars)
+                                     (apply-env saved-env search-var)
+                                     (let ((first-var (car vars)) (first-val (car vals)))
+                                       (if (eqv? first-var search-var)
+                                           first-val
+                                           (loop (cdr vars) (cdr vals) saved-env)
+                                           )
+                                       )
+                                     )
+                                 )))
+                  (loop vars vals saved-env)
+                  )
                 )
     (extend-env-rec (p-name b-var p-body saved-env)
                     (if (eqv? search-var p-name)
