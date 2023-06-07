b1385011d66291feb386ea45c15b40c5b022ec3f
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-07 19:09:01 +0800

    chore: start exer 3.10 on basis of exer 3.9

---
+++ ch3/3.2/exer-3.10/expression.rkt
@@ -0,0 +1,29 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(define-datatype expression expression?
+  (const-exp (num number?))
+  (var-exp (var symbol?))
+  (diff-exp (exp1 expression?) (exp2 expression?))
+  (zero?-exp (exp1 expression?))
+  (if-exp (exp1 expression?) (exp2 expression?) (exp3 expression?))
+  (let-exp
+   (var symbol?)
+   (exp1 expression?)
+   (body expression?)
+   )
+
+  ; new stuff
+  (cons-exp (exp1 expression?) (exp2 expression?))
+  (car-exp (exp1 expression?))
+  (cdr-exp (exp1 expression?))
+  (emptylist-exp)
+  (null?-exp (exp1 expression?))
+  )
+
+(define-datatype program program?
+  (a-program
+   (exp1 expression?)
+   )
+  )
