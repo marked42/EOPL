337f10ac7b56d5a6fdbc8e98c2d4b9e66bab1a09
penghui <penghui.zhang@foxmail.com>
2023-06-09 00:30:13 +0800

    chore(exer): start exer 7.5 on basis of checked-lang

---
+++ ch7/7.3/exer-7.5/environment.rkt
@@ -0,0 +1,58 @@
+#lang eopl
+
+(require racket/lazy-require "expression.rkt")
+(lazy-require
+ ["value.rkt" (num-val expval? proc-val)]
+ ["procedure.rkt" (procedure)]
+ )
+(provide (all-defined-out))
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+   (var symbol?)
+   (val expval?)
+   (saved-env environment?)
+   )
+  (extend-env-rec
+   (p-name symbol?)
+   (b-var symbol?)
+   (p-body expression?)
+   (saved-env environment?)
+   )
+  )
+
+(define (init-env)
+  (extend-env 'i (num-val 1)
+              (extend-env 'v (num-val 5)
+                          (extend-env 'x (num-val 10)
+                                      (empty-env)
+                                      )
+                          )
+              )
+  )
+
+(define (apply-env env search-var)
+  (cases environment env
+    (extend-env (var val saved-env)
+                (if (eqv? search-var var)
+                    val
+                    (apply-env saved-env search-var)
+                    )
+                )
+    (extend-env-rec (p-name b-var p-body saved-env)
+                    (if (eqv? search-var p-name)
+                        ; procedure env is extend-env-rec itself which contains procedure
+                        ; when procedure is called, procedure body is evaluated in this extend-env-rec
+                        ; where procedure is visible, which enables recursive call
+                        (proc-val (procedure b-var p-body env))
+                        (apply-env saved-env search-var)
+                        )
+                    )
+    (else (report-no-binding-found search-var))
+    )
+  )
+
+(define (report-no-binding-found var)
+  (eopl:error 'apply-env "No binding for ~s" var)
+  )
