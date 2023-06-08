da559e6a54cda8addbb0f1a37030f15fb65053e8
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-08 12:11:08 +0800

    chore: start exer 3.32 on basis of letrec-lang

---
+++ ch3/3.4/exer-3.32/expression.rkt
@@ -0,0 +1,25 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(define-datatype expression expression?
+  (const-exp (num number?))
+  (var-exp (var symbol?))
+  (diff-exp (exp1 expression?) (exp2 expression?))
+  (zero?-exp (exp1 expression?))
+  (if-exp (exp1 expression?) (exp2 expression?) (exp3 expression?))
+  (let-exp
+   (var symbol?)
+   (exp1 expression?)
+   (body expression?)
+   )
+  (proc-exp (var symbol?) (body expression?))
+  (call-exp (rator expression?) (rand expression?))
+  (letrec-exp (p-name symbol?) (b-var symbol?) (p-body expression?) (body expression?))
+  )
+
+(define-datatype program program?
+  (a-program
+   (exp1 expression?)
+   )
+  )
