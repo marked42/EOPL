b0c7fc20c7900b2f40fe9509a1f4843aa71b73fa
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-09 13:32:15 +0800

    refactor: update extend-env-rec* to support multiple declarations in letrec-exp

--- ch7/7.3/exer-7.5/interpreter.rkt
+++ ch7/7.3/exer-7.5/interpreter.rkt
@@ -6,7 +6,7 @@
                      init-env
                      apply-env
                      extend-env*
-                     extend-env-rec
+                     extend-env-rec*
                      )]
  ["value.rkt" (num-val expval->num bool-val expval->bool proc-val expval->proc)]
  ["procedure.rkt" (procedure apply-procedure)]
@@ -74,7 +74,7 @@
                 )
               )
     (letrec-exp (p-result-type p-name b-typed-vars p-body body)
-                (let ([new-env (extend-env-rec p-name (typed-vars->vars b-typed-vars) p-body env)])
+                (let ([new-env (extend-env-rec* (list p-name) (list (typed-vars->vars b-typed-vars)) (list p-body) env)])
                   (value-of-exp body new-env)
                   )
                 )
