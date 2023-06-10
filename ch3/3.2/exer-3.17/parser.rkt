ca07884b1f8bbd01302d74ede29f8bb63050f182
penghui <penghui.zhang@foxmail.com>
2023-06-10 15:48:04 +0800

    chore: start exer 3.17 on top of let-lang

---
+++ ch3/3.2/exer-3.17/parser.rkt
@@ -0,0 +1,30 @@
+#lang eopl
+
+(require "expression.rkt")
+(provide (all-defined-out))
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+    (expression (number) const-exp)
+    (expression (identifier) var-exp)
+
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+
+    (expression ("if" expression "then" expression "else" expression) if-exp)
+
+    (expression ("let" identifier "=" expression "in" expression) let-exp)
+    )
+  )
+
+(define scan&parse (sllgen:make-string-parser the-lexical-spec the-grammar))
