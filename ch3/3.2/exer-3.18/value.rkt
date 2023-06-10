227af95c8d224991a17c2d335d07a3ff593116a8
penghui <penghui.zhang@foxmail.com>
2023-06-10 16:04:25 +0800

    chore: start exer 3.18 on top of exer 3.9

---
+++ ch3/3.2/exer-3.18/value.rkt
@@ -0,0 +1,79 @@
+#lang eopl
+
+(require "expression.rkt")
+
+(provide (all-defined-out))
+
+(define (report-expval-extractor-error type val)
+  (eopl:error 'expval-extractors "Looking for a ~s, found ~s" type val)
+  )
+
+(define-datatype expval expval?
+  (num-val (num number?))
+  (bool-val (bool boolean?))
+  ; new stuff
+  (null-val)
+  (cell-val (first expval?) (second expval?))
+  )
+
+(define (expval->num val)
+  (cases expval val
+    (num-val (num) num)
+    (else (report-expval-extractor-error 'num val))
+    )
+  )
+
+(define (expval->bool val)
+  (cases expval val
+    (bool-val (bool) bool)
+    (else (report-expval-extractor-error 'bool val))
+    )
+  )
+
+; new stuff
+(define (cell-val->first val)
+  (cases expval val
+    (cell-val (first second) first)
+    (null-val () (null-val))
+    (else "error")
+    )
+  )
+
+(define (cell-val->second val)
+  (cases expval val
+    (cell-val (first second) second)
+    (null-val () (null-val))
+    (else "error")
+    )
+  )
+
+(define (null-val? val)
+  (cases expval val
+    (null-val () #t)
+    (else #f)
+    )
+  )
+
+(define (cell-val? val)
+  (cases expval val
+    (cell-val (first second) #t)
+    (else #f)
+    )
+  )
+
+(define sloppy->expval
+  (lambda (sloppy-val)
+    (cond
+      ((number? sloppy-val) (num-val sloppy-val))
+      ((boolean? sloppy-val) (bool-val sloppy-val))
+      ; new stuff
+      ((null? sloppy-val) (null-val))
+      ((pair? sloppy-val)
+       (let ((first (car sloppy-val)) (second (cdr sloppy-val)))
+         (cell-val (sloppy->expval first) (sloppy->expval second))
+         )
+       )
+      (else
+       (eopl:error 'sloppy->expval
+                   "Can't convert sloppy value to expval: ~s"
+                   sloppy-val)))))
