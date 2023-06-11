f5e48f33434713fddede31b8fb4d2b529f7a3d4d
penghui <penghui.zhang@foxmail.com>
2023-06-11 22:34:12 +0800

    chore: start exer 3.28 on top of proc-lang-lambda

---
+++ ch3/3.3/exer-3.28/proc-lang-lambda/procedure.rkt
@@ -0,0 +1,19 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(require racket/lazy-require "environment.rkt")
+(lazy-require
+ ["interpreter.rkt" (value-of-exp)])
+
+(define (proc? val) (procedure? val))
+
+(define (procedure var body env)
+  (lambda (val)
+    (value-of-exp body (extend-env var val env))
+    )
+  )
+
+(define (apply-procedure proc arg)
+  (proc arg)
+  )
