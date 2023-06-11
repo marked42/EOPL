fa7735201927013d114eee473a476cacbc571336
penghui <penghui.zhang@foxmail.com>
2023-06-11 23:36:47 +0800

    feat: exer 3.37 recursive procedure with dynamic scoping rule

--- ch3/3.4/exer-3.37/dynamic/test.rkt
+++ ch3/3.4/exer-3.37/dynamic/test.rkt
@@ -9,5 +9,16 @@
         test-cases-proc-lang
         test-cases-let-exp-with-multiple-declarations
         test-cases-dynamic-scoping
+        (list
+            (list "
+let fact = proc (n) add1(n)
+    in let fact = proc (n)
+                        if zero?(n)
+                        then 1
+                        % fact refers to fact at line 2 under dynamic scoping
+                        else *(n, (fact -(n,1)))
+            in (fact 5)
+            " 120 "recurisve expression by dynamic scoping")
+        )
     )
 )
