067e44a470e3020a95c7c56673d695e4852c7d7e
penghui <penghui.zhang@foxmail.com>
2023-06-11 21:56:18 +0800

    feat(exer): exer 3.36
    support letrec-exp with multiple declarations using environment of
    procedural representation and single element vector to avoid creating
    same procedure multiple times in letrec-exp.

--- ch3/3.4/exer-3.36/expression.rkt
+++ ch3/3.4/exer-3.36/expression.rkt
@@ -15,7 +15,7 @@
    )
   (proc-exp (var symbol?) (body expression?))
   (call-exp (rator expression?) (rand expression?))
-  (letrec-exp (p-name symbol?) (b-var symbol?) (p-body expression?) (body expression?))
+  (letrec-exp (p-names (list-of symbol?)) (b-vars (list-of symbol?)) (p-bodies (list-of expression?)) (body expression?))
   )
 
 (define-datatype program program?
