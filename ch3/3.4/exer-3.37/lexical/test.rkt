d6f0bde9927c14f4428d82beb01c3ff2c3441165
penghui <penghui.zhang@foxmail.com>
2023-06-11 23:43:42 +0800

    feat: exer 3.37 example under lexical scoping

--- ch3/3.4/exer-3.37/lexical/test.rkt
+++ ch3/3.4/exer-3.37/lexical/test.rkt
@@ -2,6 +2,21 @@
 
 (require "interpreter.rkt")
 (require "value.rkt")
-(require "../../../base/test.rkt")
+(require "../../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-proc-lang)
+(test-lang run sloppy->expval
+    (append
+        test-cases-proc-lang
+        (list
+            (list "
+let fact = proc (n) add1(n)
+    in let fact = proc (n)
+                        if zero?(n)
+                        then 1
+                        % fact refers to fact at line under lexical scoping
+                        else *(n, (fact -(n,1)))
+            in (fact 5)
+            " 25 "lexical scoping")
+        )
+    )
+)
