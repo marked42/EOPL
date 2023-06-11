1d4bbaf2d369bff2a6d0e9b8c9c79227ec461b33
penghui <penghui.zhang@foxmail.com>
2023-06-11 22:49:52 +0800

    feat(exer): exer 3.28 dynamic scoping for procedure call
    procedure represented with native procedure

--- ch3/3.3/exer-3.28/proc-lang-lambda/test.rkt
+++ ch3/3.3/exer-3.28/proc-lang-lambda/test.rkt
@@ -4,4 +4,10 @@
 (require "value.rkt")
 (require "../../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-proc-lang)
+(test-lang run sloppy->expval
+    (append
+        test-cases-proc-lang
+        test-cases-let-exp-with-multiple-declarations
+        test-cases-dynamic-scoping
+    )
+)
