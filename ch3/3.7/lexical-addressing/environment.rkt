5ad7d80699f076df399a52edf0639b3409a25c2b
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-12 10:24:16 +0800

    chore: format lexical adressing

--- ch3/3.7/lexical-addressing/environment.rkt
+++ ch3/3.7/lexical-addressing/environment.rkt
@@ -10,18 +10,18 @@
 
 (define (extend-namless-env val saved-env)
   (cons val saved-env)
-)
+  )
 
 (define (nameless-environment? env) ((list-of expval?) env))
 
 (define (init-nameless-env)
   (extend-namless-env (num-val 1)
-              (extend-namless-env (num-val 5)
-                          (extend-namless-env (num-val 10)
-                                      (empty-nameless-env)
-                                      )
-                          )
-              )
+                      (extend-namless-env (num-val 5)
+                                          (extend-namless-env (num-val 10)
+                                                              (empty-nameless-env)
+                                                              )
+                                          )
+                      )
   )
 
 (define (apply-nameless-env env offset)
