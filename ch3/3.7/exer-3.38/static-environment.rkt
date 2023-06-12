31a1fea116f6854729c1a17503be422e69a8e954
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-12 10:30:06 +0800

    chore: start exer 3.38 on top of lexcial-addressing

---
+++ ch3/3.7/exer-3.38/static-environment.rkt
@@ -0,0 +1,29 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(define (empty-env) '())
+
+(define (init-senv)
+  (extend-senv 'i
+               (extend-senv 'v
+                            (extend-senv 'x (empty-env))
+                            )
+               )
+  )
+
+(define (extend-senv var senv)
+  (cons var senv)
+  )
+
+(define (apply-senv senv var)
+  (cond
+    [(null? senv) (report-unbound-var var)]
+    [(eqv? (car senv) var) 0]
+    [else (+ 1 (apply-senv (cdr senv) var))]
+    )
+  )
+
+(define (report-unbound-var var)
+  (eopl:error 'apply-senv "No binding for ~s" var)
+  )
