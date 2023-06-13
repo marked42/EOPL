7409bafa38a90ce0722b475cb6df1a31f12e1e30
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-13 18:26:34 +0800

    refactor: move test-has-binding? to shared file

--- ch2/test.rkt
+++ ch2/test.rkt
@@ -28,3 +28,12 @@
   (check-equal? (empty-env? (empty-env)) #t "empty-env?")
   (check-equal? (empty-env? (extend-env 'i 1 (empty-env))) #f "empty-env?")
   )
+
+(define (test-has-binding? empty-env extend-env has-binding?)
+  (let ([env (extend-env 'i 1 (extend-env 'v 5 (extend-env 'x 10 (empty-env))))])
+    (check-equal? (has-binding? env 'i) #t "has-binding? i should be true")
+    (check-equal? (has-binding? env 'v) #t "has-binding? v should be true")
+    (check-equal? (has-binding? env 'x) #t "has-binding? x should be true")
+    (check-equal? (has-binding? env 'non-exist-var) #f "has-binding non-exist-var should be false")
+    )
+  )
