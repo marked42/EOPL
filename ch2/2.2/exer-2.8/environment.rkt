e7c672c7ce0697257a15d3d51766173a8a3a0761
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-13 13:27:44 +0800

    feat: exer 2.8 add empty-env?
    based on top of exer 2.5

---
+++ ch2/2.2/exer-2.8/environment.rkt
@@ -0,0 +1,34 @@
+#lang eopl
+
+(require racket/list)
+(provide (all-defined-out))
+
+(define (empty-env) '())
+
+(define (extend-env var val env)
+  (cons (cons var val) env)
+  )
+
+; new stuff
+(define (empty-env? env) (null? env))
+
+(define (apply-env env search-var)
+  (if (null? env)
+      (report-no-binding-found search-var)
+      (let* ([top (first env)] [saved-var (car top)] [saved-val (cdr top)])
+        (if (eqv? search-var saved-var)
+            saved-val
+            (apply-env (cdr env) search-var)
+            )
+        )
+      )
+  )
+
+(define (report-no-binding-found search-var)
+  (eopl:error 'apply-env "No binding for ~s" search-var)
+  )
+
+
+(define (report-invalid-env env)
+  (eopl:error 'apply-env "Bad environment: ~s" env)
+  )
