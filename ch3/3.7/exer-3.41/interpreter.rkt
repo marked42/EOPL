06bf5034f282282b7f012eb7d5cdeb3b05347fa0
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-13 19:22:25 +0800

    chore: start exer 3.41 on top of lexical-addressing

---
+++ ch3/3.7/exer-3.41/interpreter.rkt
@@ -0,0 +1,79 @@
+#lang eopl
+
+(require racket/lazy-require "parser.rkt" "expression.rkt")
+(lazy-require
+ ["environment.rkt" (
+                     init-nameless-env
+                     apply-nameless-env
+                     extend-nameless-env
+                     )]
+ ["value.rkt" (num-val expval->num bool-val expval->bool proc-val expval->proc)]
+ ["procedure.rkt" (procedure apply-procedure)]
+ ["translator.rkt" (translation-of-program)]
+ )
+
+(provide (all-defined-out))
+
+(define (run str)
+  (let ([translated-prog (translation-of-program (scan&parse str))])
+    (value-of-program translated-prog)
+    )
+  )
+
+(define (value-of-program prog)
+  (cases program prog
+    (a-program (exp1) (value-of-exp exp1 (init-nameless-env)))
+    )
+  )
+
+(define (value-of-exp exp env)
+  (cases expression exp
+    (const-exp (num) (num-val num))
+    (diff-exp (exp1 exp2)
+              (let ([val1 (value-of-exp exp1 env)]
+                    [val2 (value-of-exp exp2 env)])
+                (let ((num1 (expval->num val1))
+                      (num2 (expval->num val2)))
+                  (num-val (- num1 num2))
+                  )
+                )
+              )
+    (zero?-exp (exp1)
+               (let ([val (value-of-exp exp1 env)])
+                 (let ([num (expval->num val)])
+                   (if (zero? num)
+                       (bool-val #t)
+                       (bool-val #f)
+                       )
+                   )
+                 )
+               )
+    (if-exp (exp1 exp2 exp3)
+            (let ([val1 (value-of-exp exp1 env)])
+              (if (expval->bool val1)
+                  (value-of-exp exp2 env)
+                  (value-of-exp exp3 env)
+                  )
+              )
+            )
+    (call-exp (rator rand)
+              (let ([rator-val (value-of-exp rator env)] [rand-val (value-of-exp rand env)])
+                (let ((proc1 (expval->proc rator-val)))
+                  (apply-procedure proc1 rand-val)
+                  )
+                )
+              )
+
+    ; new stuff
+    (nameless-var-exp (num) (apply-nameless-env env num))
+    (nameless-let-exp (exp1 body)
+                      (let ([val (value-of-exp exp1 env)])
+                        (value-of-exp body (extend-nameless-env val env))
+                        )
+                      )
+    (nameless-proc-exp (body)
+                       (proc-val (procedure body env))
+                       )
+    (else (eopl:error 'value-of-exp "unsupported expression type ~s" exp))
+    )
+  )
---
+++ ch3/3.7/exer-3.41/translator.rkt
@@ -0,0 +1,61 @@
+#lang eopl
+
+(require racket/lazy-require "expression.rkt")
+(lazy-require
+ ["static-environment.rkt" (
+                            init-senv
+                            apply-senv
+                            extend-senv
+                            )]
+ )
+
+(provide (all-defined-out))
+
+(define (translation-of-program prog)
+  (cases program prog
+    (a-program (exp1)
+               (a-program (translation-of-exp exp1 (init-senv)))
+               )
+    )
+  )
+
+(define (translation-of-exp exp senv)
+  (cases expression exp
+    (const-exp (num) exp)
+    (diff-exp (exp1 exp2)
+              (diff-exp
+               (translation-of-exp exp1 senv)
+               (translation-of-exp exp2 senv)
+               )
+              )
+    (zero?-exp (exp1)
+               (zero?-exp (translation-of-exp exp1 senv))
+               )
+    (if-exp (exp1 exp2 exp3)
+            (if-exp
+             (translation-of-exp exp1 senv)
+             (translation-of-exp exp2 senv)
+             (translation-of-exp exp3 senv)
+             )
+            )
+    (var-exp (var) (nameless-var-exp (apply-senv senv var)))
+    (let-exp (var exp1 body)
+             (nameless-let-exp
+              (translation-of-exp exp1 senv)
+              (translation-of-exp body (extend-senv var senv))
+              )
+             )
+    (proc-exp (var body)
+              (nameless-proc-exp
+               (translation-of-exp body (extend-senv var senv))
+               )
+              )
+    (call-exp (rator rand)
+              (call-exp
+               (translation-of-exp rator senv)
+               (translation-of-exp rand senv)
+               )
+              )
+    (else (eopl:error 'translation-of-exp "unsupported expression type ~s" exp))
+    )
+  )
