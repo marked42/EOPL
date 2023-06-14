de0c0466687d4db27223a354d004e353de67d4d5
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-14 18:33:01 +0800

    feat: add printer for easy testing

--- ch3/3.7/exer-3.43/test.rkt
+++ ch3/3.7/exer-3.43/test.rkt
@@ -1,7 +1,27 @@
 #lang eopl
 
-(require "interpreter.rkt")
+(require rackunit "interpreter.rkt" "parser.rkt" "printer.rkt" "translator.rkt")
 (require "value.rkt")
 (require "../../../base/test.rkt")
 
 (test-lang run sloppy->expval test-cases-proc-lang)
+
+(check-equal?
+ (print-program (scan&parse "
+let x = 3
+    in let f = proc (y) -(y,x)
+        in (f 13)
+"))
+ "let x = 3 in let f = proc (y) -(y, x) in (f 13)"
+ "print-program"
+ )
+
+(check-eq?
+ (print-program (translation-of-program (scan&parse "
+let x = 3
+    in let f = proc (y) -(y,x)
+        in (f 13)
+")))
+ "%let 3 in %let %lexproc -(%lexref 0, %lexref 1) in (%lexref 0 13)"
+ "print-program"
+ )
