59fb648f986a15e39f42c8ba4db097bdd23420b5
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-14 19:19:24 +0800

    feat: exer 3.43
    inline variable that references known procedures with procedure definition,
    adjust procedure body internal var depth.

--- ch3/3.7/exer-3.43/static-environment.rkt
+++ ch3/3.7/exer-3.43/static-environment.rkt
@@ -2,14 +2,15 @@
 
 (provide (all-defined-out))
 
+(require "expression.rkt")
 (define (empty-env) '())
 
 (define (init-senv)
   (extend-senv-normal 'i
-               (extend-senv-normal 'v
-                            (extend-senv-normal 'x (empty-env))
-                            )
-               )
+                      (extend-senv-normal 'v
+                                          (extend-senv-normal 'x (empty-env))
+                                          )
+                      )
   )
 
 (define (extend-senv var val senv)
@@ -18,19 +19,33 @@
 
 (define (extend-senv-normal var senv)
   (extend-senv var #f senv)
-)
+  )
 
 (define (apply-senv senv var)
   (let loop ([senv senv] [depth 0])
     (if (null? senv)
-      (report-unbound-var var)
-      (let ([saved-var (caar senv)] [saved-val (cdar senv)])
-        (if (eqv? saved-var var)
-          (cons depth saved-val)
-          (loop (cdr senv) (+ depth 1))
+        (report-unbound-var var)
+        (let ([saved-var (caar senv)] [saved-val (cdar senv)])
+          (if (eqv? saved-var var)
+              (cons depth saved-val)
+              (loop (cdr senv) (+ depth 1))
+              )
           )
         )
-      )
+    )
+  )
+
+(define (apply-senv-non-proc senv var)
+  (let loop ([senv senv] [depth 0])
+    (if (null? senv)
+        (report-unbound-var var)
+        (let ([saved-var (caar senv)] [saved-val (cdar senv)])
+          (if (eqv? saved-var var)
+              depth
+              (loop (cdr senv) (+ depth (if (is-proc-exp? saved-val) 0 1)))
+              )
+          )
+        )
     )
   )
 
