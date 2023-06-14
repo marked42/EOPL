53fc19a3168dd4bd88b4da793a8529c6cf484075
penghui <penghui.zhang@foxmail.com>
2023-06-14 23:51:56 +0800

    feat: exer 3.44 compiles away useless let-exp that declares procedure
    update translation of let-exp/var-exp

--- ch3/3.7/exer-3.44/test.rkt
+++ ch3/3.7/exer-3.44/test.rkt
@@ -36,10 +36,7 @@ let x = 3
     in let f = proc (y) -(y, x)
         in (f 13)
 ")))
- ;  "%let 3 in %let %lexproc -(%lexref 0, %lexref 1) in (%lexref 0 13)"
- ; f in inlined with its definition, depth of variable x is adjusted from
- ; 1 (depth at def site) to 2 (depth at call site) by 1 (offset of f)
- "%let 3 in %let %lexproc -(%lexref 0, %lexref 1) in (%lexproc -(%lexref 0, %lexref 2) 13)"
+ "%let 3 in (%lexproc -(%lexref 0, %lexref 1) 13)"
  "print-program"
  )
 
@@ -51,6 +48,6 @@ let x = 3
             in let g = proc (z) -(z, p)
                 in -((f 13), (g 14))
 ")))
- "%let 3 in %let 4 in %let %lexproc -(%lexref 0, %lexref 2) in %let %lexproc -(%lexref 0, %lexref 2) in -((%lexproc -(%lexref 0, %lexref 4) 13), (%lexproc -(%lexref 0, %lexref 3) 14))"
+ "%let 3 in %let 4 in -((%lexproc -(%lexref 0, %lexref 2) 13), (%lexproc -(%lexref 0, %lexref 1) 14))"
  "print-program"
  )
