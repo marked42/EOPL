d8a2f5520d4514544a4e6ce89d7526a4862ad914
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-19 13:50:55 +0800

    feat: exer 4.16

--- ch4/4.3/exer-4.16/test.rkt
+++ ch4/4.3/exer-4.16/test.rkt
@@ -4,4 +4,25 @@
 (require "value.rkt")
 (require "../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-implicit-refs-lang)
+(test-lang run sloppy->expval
+           (append
+            test-cases-implicit-refs-lang
+            (list
+             (list "
+letrec times4(x) = if zero?(x) then 0 else -((times4 -(x,1)), -4)
+    in (times4 3)
+    " 12 "recursive by letrec")
+             (list "
+let times4 = 0
+      in begin
+            set times4 = proc (x)
+                            if zero?(x)
+                            then 0
+                            else -((times4 -(x,1)), -4);
+            (times4 3)
+        end
+
+             " 12 "recursive by assignment")
+             )
+            )
+           )
