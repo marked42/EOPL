61b6ecaf1a8145404d64c35bf8fe1254dfb2c559
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-06-19 11:41:09 +0800

    feat: exer 4.12
    specify effects represented by store of each expression clearly by passing
    store between different prcocedures.
    
    see chapter 4.2.1 Store-Passing Specifications

--- ch4/4.2/exer-4.12/store.rkt
+++ ch4/4.2/exer-4.12/store.rkt
@@ -1,38 +1,32 @@
 #lang eopl
 
-(require racket/list)
+(require racket/list "value.rkt")
 
 (provide (all-defined-out))
 
+(define store? (list-of expval?))
+
 (define (empty-store) '())
-
-(define the-store 'uninitialized)
-
-(define (get-store) the-store)
-
-(define (initialize-store!)
-  (set! the-store (empty-store))
-  )
-
-(define (show-store)
-  (eopl:pretty-print (list "store is: " the-store))
-  )
 
 (define (reference? v) (integer? v))
 
-(define (newref val)
-  (let ([next-ref (length the-store)])
-    (set! the-store (append the-store (list val)))
-    next-ref
-    )
+(define (newref the-store val)
+  (cons
+   (append the-store (list val))
+   (length the-store)
+   )
   )
 
-(define (deref ref)
-  (list-ref the-store ref)
+(define (deref the-store ref)
+  (cons
+   the-store
+   (list-ref the-store ref)
+   )
   )
 
-(define (setref! ref val)
-  (set! the-store (list-set the-store ref val))
+(define (setref! the-store ref val)
+  ; return an arbitrary val since we don't care the return value of setref!
+  (cons (list-set the-store ref val) (num-val 23))
   )
 
 (define (report-invalid-reference ref the-store)
