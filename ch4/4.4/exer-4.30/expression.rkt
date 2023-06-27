4c2285662f5f2e8067ea91d288252cce81474a31
penghui <penghui.zhang@foxmail.com>
2023-06-28 00:38:44 +0800

    chore: start exer 4.30 on top of exer 4.29

---
+++ ch4/4.4/exer-4.30/expression.rkt
@@ -0,0 +1,34 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(define-datatype expression expression?
+  (const-exp (num number?))
+  (var-exp (var symbol?))
+  (diff-exp (exp1 expression?) (exp2 expression?))
+  (zero?-exp (exp1 expression?))
+  (if-exp (exp1 expression?) (exp2 expression?) (exp3 expression?))
+  (let-exp
+   (var symbol?)
+   (exp1 expression?)
+   (body expression?)
+   )
+  (proc-exp (var symbol?) (body expression?))
+  (call-exp (rator expression?) (rand expression?))
+  (letrec-exp (p-names (list-of symbol?)) (b-vars (list-of symbol?)) (p-bodies (list-of expression?)) (body expression?))
+
+  (begin-exp (exp1 expression?) (exps (list-of expression?)))
+
+  (assign-exp (var symbol?) (exp1 expression?))
+
+  ; new stuff
+  (newarray-exp (exp1 expression?) (exp2 expression?))
+  (arrayref-exp (exp1 expression?) (exp2 expression?))
+  (arrayset-exp (exp1 expression?) (exp2 expression?) (exp3 expression?))
+  )
+
+(define-datatype program program?
+  (a-program
+   (exp1 expression?)
+   )
+  )
---
+++ ch4/4.4/exer-4.30/store.rkt
@@ -0,0 +1,44 @@
+#lang eopl
+
+(require racket/list)
+
+(provide (all-defined-out))
+
+(define (empty-store) '())
+
+(define the-store 'uninitialized)
+
+(define (get-store) the-store)
+
+(define (initialize-store!)
+  (set! the-store (empty-store))
+  )
+
+(define (show-store)
+  (eopl:pretty-print (list "store is: " the-store))
+  )
+
+(define (reference? v) (integer? v))
+
+(define (next-ref)
+  (length the-store)
+)
+
+(define (newref val)
+  (let ([next-ref (length the-store)])
+    (set! the-store (append the-store (list val)))
+    next-ref
+    )
+  )
+
+(define (deref ref)
+  (list-ref the-store ref)
+  )
+
+(define (setref! ref val)
+  (set! the-store (list-set the-store ref val))
+  )
+
+(define (report-invalid-reference ref the-store)
+  (eopl:error 'setref! "illegal reference ~s in store ~s" ref the-store)
+  )
