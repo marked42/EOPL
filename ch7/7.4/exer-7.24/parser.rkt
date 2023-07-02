6a23a32a270a3dad2368373583d8b10d9c850f2a
penghui <penghui.zhang@foxmail.com>
2023-07-02 15:56:58 +0800

    feat: support let with multiple declarations
    exer 7.24

--- ch7/7.4/exer-7.24/parser.rkt
+++ ch7/7.4/exer-7.24/parser.rkt
@@ -24,7 +24,7 @@
 
     (expression ("if" expression "then" expression "else" expression) if-exp)
 
-    (expression ("let" identifier "=" expression "in" expression) let-exp)
+    (expression ("let" (arbno identifier "=" expression) "in" expression) let-exp)
 
     (expression ("proc" "(" identifier ":" optional-type")" expression) proc-exp)
     (expression ("("expression expression")" ) call-exp)
