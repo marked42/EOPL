14710849863f5d78b1210c8594e66e25f521c936
penghui <penghui.zhang@foxmail.com>
2023-07-02 15:35:54 +0800

    refactor: support multiple variables in single type environment record
    update extend-tenv to extend-tenv*

--- ch7/7.4/exer-7.24/inferrer/type-environment.rkt
+++ ch7/7.4/exer-7.24/inferrer/type-environment.rkt
@@ -1,34 +1,34 @@
 #lang eopl
 
+(require "type.rkt" racket/list)
+
 (provide (all-defined-out))
-(require "type.rkt")
 
 (define-datatype type-environment type-environment?
   (empty-tenv)
-  (extend-tenv
-   (var symbol?)
-   (type type?)
+  (extend-tenv*
+   (vars (list-of symbol?))
+   (type (list-of type?))
    (tenv type-environment?)
    )
   )
 
 (define (init-tenv)
-  (extend-tenv 'i (int-type)
-               (extend-tenv 'v (int-type)
-                            (extend-tenv 'x (int-type)
-                                         (empty-tenv)
-                                         )
-                            )
+  (extend-tenv* '(i v x)
+               (list (int-type) (int-type) (int-type))
+               (empty-tenv)
                )
   )
 
 (define (apply-tenv env search-var)
   (cases type-environment env
-    (extend-tenv (var type saved-env)
-                 (if (eqv? search-var var)
-                     type
-                     (apply-tenv saved-env search-var)
-                     )
+    (extend-tenv* (vars types saved-env)
+                 (let ([index (index-of vars search-var)])
+                   (if index
+                       (list-ref types index)
+                       (apply-tenv saved-env search-var)
+                       )
+                   )
                  )
     (else (eopl:error "Unbound variable ~s" search-var))
     )
