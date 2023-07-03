1ba845780212c416312bc3e1302508dba56c7ea0
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-03 17:05:10 +0800

    feat: type inference for explicit-refs
    newref/deref/setref expression
    
    exer 7.26

--- ch7/7.4/exer-7.26/test.rkt
+++ ch7/7.4/exer-7.26/test.rkt
@@ -175,6 +175,51 @@ letrec ? fact(x: ?) = if zero?(x) then 1 else -(x, (fact -(x,1)))
    )
   )
 
+(define test-cases-ref-exp
+  (list
+   (list "
+    let a = newref(43)
+      in a
+    " '(ref int) "newref create ref type")
+
+   (list "
+    let a = newref(43)
+      in deref(a)
+    " 'int "deref unwraps ref type of int")
+
+   (list "
+    let a = newref(zero?(1))
+      in deref(a)
+    " 'bool "deref unwraps ref type of bool")
+
+   (list "
+     let a = newref(43)
+       in let b = newref(42)
+         in -(deref(a), deref(b))
+     " 'int "deref retrieves value of a ref")
+
+   (list "
+     let a = newref(42)
+       in setref(a, 43)
+     " 'void "setref! returns void type")
+
+   (list "
+     let a = newref(42)
+       in let b = setref(a, 43)
+         in deref(a)
+     " 'int "setref! updates value of a ref int")
+
+   (list "
+     let a = newref(42)
+       in setref(a, zero?(0))
+     " 'error "setref! throw error when type mismatch, set bool to (ref int)")
+
+   (list "let a = 1 in (proc (x: refto int) deref(x) newref(a))" 'int "proc with parameter of ref type")
+   (list "let a = 1 in (proc (x: refto int) x a)" 'error "type int and ref type of int are not same type")
+   (list "let a = 1 in (proc (x: refto bool) x a)" 'error "type bool and ref type int are not same type")
+   )
+  )
+
 (define test-cases-inferrer-lang
   (append
    test-cases-simple-arithmetic
@@ -189,6 +234,8 @@ letrec ? fact(x: ?) = if zero?(x) then 1 else -(x, (fact -(x,1)))
    test-cases-procedures
    test-cases-circular-types
    test-cases-polymorphic
+
+   test-cases-ref-exp
    )
   )
 
