730029b17c9e3ee0f702ae501f11deaab54b4ec9
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-04 11:42:06 +0800

    feat: type inference in two steps
    collect all type equations first, then solve equations using unification algorithm
    and get resulting type of whole expression.
    
    exer 7.27

--- ch7/7.4/exer-7.27/inferrer/unifier.rkt
+++ ch7/7.4/exer-7.27/inferrer/unifier.rkt
@@ -1,35 +1,73 @@
 #lang eopl
 
-(require "substitution.rkt" "type.rkt")
+(require racket/list "substitution.rkt" "type.rkt" "../expression.rkt")
 
 (provide (all-defined-out))
 
-(define (unifier ty1 ty2 subst exp)
-  (let ([ty1 (apply-subst-to-type ty1 subst)] [ty2 (apply-subst-to-type ty2 subst)])
-    (cond
-      [(equal? ty1 ty2) subst]
-      [(tvar-type? ty1)
-       (if (no-occurrence? ty1 ty2)
-           (extend-subst subst ty1 ty2)
-           (report-no-occurrence-violation ty1 ty2 exp)
-           )
-       ]
-      [(tvar-type? ty2)
-       (if (no-occurrence? ty2 ty1)
-           (extend-subst subst ty2 ty1)
-           (report-no-occurrence-violation ty2 ty1 exp)
-           )
-       ]
-      [(and (proc-type? ty1) (proc-type? ty2))
-       (let ([subst (unifier (proc-type->arg-type ty1) (proc-type->arg-type ty2) subst exp)])
-         (let ([subst (unifier (proc-type->result-type ty1) (proc-type->result-type ty2) subst exp)])
-           subst
-           )
-         )
-       ]
-      [else (report-unification-failure ty1 ty2 exp)]
-      )
+(define (unify equations)
+  (let loop ([equations equations] [subst '()])
+    (if (null? equations)
+        subst
+        (let* ([next-equation (car equations)]
+               [ty1 (first next-equation)]
+               [ty2 (second next-equation)]
+               [exp (third next-equation)])
+          (let ([ty1 (apply-subst-to-type ty1 subst)] [ty2 (apply-subst-to-type ty2 subst)])
+            (cond
+              [(equal? ty1 ty2) (loop (cdr equations) subst)]
+              [(tvar-type? ty1)
+               (if (no-occurrence? ty1 ty2)
+                   ; new substitutions found
+                   (loop (cdr equations) (extend-subst subst ty1 ty2))
+                   (report-no-occurrence-violation ty1 ty2 exp)
+                   )
+               ]
+              [(tvar-type? ty2)
+               (if (no-occurrence? ty2 ty1)
+                   ; new substitutions found
+                   (loop (cdr equations) (extend-subst subst ty2 ty1))
+                   (report-no-occurrence-violation ty2 ty1 exp)
+                   )
+               ]
+              [(and (proc-type? ty1) (proc-type? ty2))
+               ; new equations added
+               (let ([equations
+                      (extend-equations
+                       (proc-type->arg-type ty1)
+                       (proc-type->arg-type ty2)
+                       (extend-equations
+                        (proc-type->result-type ty1)
+                        (proc-type->result-type ty2)
+                        equations
+                        exp
+                        )
+                       exp
+                       )
+                      ])
+                 (loop equations subst)
+                 )
+               ]
+              [else (report-unification-failure ty1 ty2 exp)]
+              )
+            )
+          )
+        )
     )
+  )
+
+(define equations? (list-of (lambda (val)
+                              (and
+                               (list? val)
+                               (type? (first val))
+                               (type? (second val))
+                               (expression? (third val))
+                               )
+                              )))
+
+(define (empty-equations) '())
+
+(define (extend-equations ty1 ty2 equations exp)
+  (cons (list ty1 ty2 exp) equations)
   )
 
 (define (report-no-occurrence-violation ty1 ty2 exp)
