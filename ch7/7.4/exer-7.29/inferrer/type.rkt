44a420fecd4c659b0a3e382fe035c0e9064baebf
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-05 16:21:31 +0800

    chore: start exer 7.29 on top of inferred-lang

---
+++ ch7/7.4/exer-7.29/inferrer/type.rkt
@@ -0,0 +1,86 @@
+#lang eopl
+
+(provide (all-defined-out))
+
+(provide (all-defined-out))
+
+(define-datatype type type?
+  (int-type)
+  (bool-type)
+  (proc-type (arg-type type?) (result-type type?))
+  (tvar-type (sn integer?))
+  )
+
+(define (atomic-type? ty)
+  (cases type ty
+    (int-type () #t)
+    (bool-type () #t)
+    (else #f)
+    )
+  )
+
+(define (tvar-type? ty)
+  (cases type ty
+    (tvar-type (sn) #t)
+    (else #f)
+    )
+  )
+
+(define (proc-type? ty)
+  (cases type ty
+    (proc-type (arg-type result-type) #t)
+    (else #f)
+    )
+  )
+
+(define (proc-type->arg-type ty)
+  (cases type ty
+    (proc-type (arg-type result-type) arg-type)
+    (else (eopl:error 'proc-type->arg-type "Not a proc-type: ~s" ty))
+    )
+  )
+
+(define (proc-type->result-type ty)
+  (cases type ty
+    (proc-type (arg-type result-type) result-type)
+    (else (eopl:error 'proc-type->arg-type "Not a proc-type: ~s" ty))
+    )
+  )
+
+(define-datatype optional-type optional-type?
+  (no-type)
+  (a-type (ty type?))
+  )
+
+(define (otype->type otype)
+  (cases optional-type otype
+    (no-type () (fresh-var-type))
+    (a-type (ty) ty)
+    )
+  )
+
+(define sn 0)
+(define (fresh-var-type)
+  (set! sn (+ sn 1))
+  (tvar-type sn)
+  )
+
+(define (reset-fresh-var)
+  (set! sn 0)
+  )
+
+(define (type-to-external-form ty)
+  (cases type ty
+    (int-type () 'int)
+    (bool-type () 'bool)
+    (proc-type (arg-type result-type)
+               (list (type-to-external-form arg-type)
+                     '->
+                     (type-to-external-form result-type)
+                     )
+               )
+    (tvar-type (serial-number)
+               (string->symbol (string-append "tvar" (number->string serial-number)))
+               )
+    )
+  )
---
+++ ch7/7.4/exer-7.29/parser.rkt
@@ -0,0 +1,44 @@
+#lang eopl
+
+(require "expression.rkt")
+(require "inferrer/type.rkt")
+(provide (all-defined-out))
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+    (expression (number) const-exp)
+    (expression (identifier) var-exp)
+
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+
+    (expression ("if" expression "then" expression "else" expression) if-exp)
+
+    (expression ("let" identifier "=" expression "in" expression) let-exp)
+
+    (expression ("proc" "(" identifier ":" optional-type")" expression) proc-exp)
+    (expression ("("expression expression")" ) call-exp)
+
+    (expression ("letrec" optional-type identifier "(" identifier ":" optional-type ")" "=" expression "in" expression) letrec-exp)
+
+    (optional-type ("?") no-type)
+    (optional-type (type) a-type)
+
+    (type ("int") int-type)
+    (type ("bool") bool-type)
+    (type ("(" type "->" type")") proc-type)
+    (type ("%tvar-type" number) tvar-type)
+    )
+  )
+
+(define scan&parse (sllgen:make-string-parser the-lexical-spec the-grammar))
