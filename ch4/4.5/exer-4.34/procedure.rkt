298ea790aca1aa63456084e0a78af5c8712fb4e0
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-10 20:20:09 +0800

    chore: start exer 4.34 on top of call-by-reference

---
+++ ch4/4.5/exer-4.34/procedure.rkt
@@ -0,0 +1,27 @@
+#lang eopl
+
+(require racket/lazy-require "environment.rkt")
+(lazy-require
+ ["expression.rkt" (expression?)]
+ ["interpreter.rkt" (value-of-exp)]
+ ["store.rkt" (newref)]
+ )
+
+(provide (all-defined-out))
+
+(define-datatype proc proc?
+  (procedure
+   (var symbol?)
+   (body expression?)
+   (saved-env environment?)
+   )
+  )
+
+(define (apply-procedure proc1 arg)
+  (cases proc proc1
+    (procedure (var body saved-env)
+               ; new stuff
+               (value-of-exp body (extend-env var arg saved-env))
+               )
+    )
+  )
