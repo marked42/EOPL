2770790f36f01760158f347757517a7508bc5c9b
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-10 20:05:43 +0800

    chore: start exer 4.33 on top of call-by-reference

---
+++ ch4/4.5/exer-4.33/value.rkt
@@ -0,0 +1,50 @@
+#lang eopl
+
+(require racket/lazy-require "expression.rkt")
+(lazy-require
+ ["procedure.rkt" (proc?)]
+ )
+
+(provide (all-defined-out))
+
+(define (report-expval-extractor-error type val)
+  (eopl:error 'expval-extractors "Looking for a ~s, found ~s" type val)
+  )
+
+(define-datatype expval expval?
+  (num-val (num number?))
+  (bool-val (bool boolean?))
+  (proc-val (proc1 proc?))
+  )
+
+(define (expval->num val)
+  (cases expval val
+    (num-val (num) num)
+    (else (report-expval-extractor-error 'num val))
+    )
+  )
+
+(define (expval->bool val)
+  (cases expval val
+    (bool-val (bool) bool)
+    (else (report-expval-extractor-error 'bool val))
+    )
+  )
+
+(define (expval->proc val)
+  (cases expval val
+    (proc-val (proc1) proc1)
+    (else (report-expval-extractor-error 'proc val))
+    )
+  )
+
+
+(define sloppy->expval
+  (lambda (sloppy-val)
+    (cond
+      ((number? sloppy-val) (num-val sloppy-val))
+      ((boolean? sloppy-val) (bool-val sloppy-val))
+      (else
+       (eopl:error 'sloppy->expval
+                   "Can't convert sloppy value to expval: ~s"
+                   sloppy-val)))))
