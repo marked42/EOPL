0fba3d0b9e7427baa586d28e6283949410298091
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-10 14:42:46 +0800

    chore: start exer 4.26 on top of exer 4.25

---
+++ ch4/4.3/exer-4.26/parser.rkt
@@ -0,0 +1,44 @@
+#lang eopl
+
+(require "expression.rkt")
+(provide (all-defined-out))
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (statement) a-program)
+
+    (statement (identifier "=" expression) assign-statement)
+    (statement ("print" expression) print-statement)
+    (statement ("{" (separated-list statement ";") "}") block-statement)
+    (statement ("if" expression statement statement) if-statement)
+    (statement ("while" expression statement) while-statement)
+    (statement ("do-while" expression statement) do-while-statement)
+    (statement ("var" (separated-list var-declaration ",") ";" statement) var-statement)
+    (statement ("read" identifier) read-statement)
+
+    (var-declaration (identifier "=" expression) a-var-decl)
+
+    (expression (number) const-exp)
+    (expression (identifier) var-exp)
+
+    (expression ("-" "(" expression "," expression ")") diff-exp)
+    (expression ("+" "(" expression "," expression ")") sum-exp)
+    (expression ("*" "(" expression "," expression ")") mul-exp)
+    (expression ("zero?" "(" expression ")") zero?-exp)
+    (expression ("not" "(" expression ")") not-exp)
+
+    (expression ("proc" "(" (separated-list identifier ",") ")" expression) proc-exp)
+    (expression ("("expression (arbno expression)")" ) call-exp)
+    )
+  )
+
+(define scan&parse (sllgen:make-string-parser the-lexical-spec the-grammar))
---
+++ ch4/4.3/exer-4.26/test.rkt
@@ -0,0 +1,73 @@
+#lang eopl
+
+(require "interpreter.rkt")
+(require "value.rkt")
+(require "../../../base/test.rkt")
+
+(define test-cases-statement
+  (list
+   (list "x = 3" 3 "assign statement")
+
+   (list "print 3" 3 "print statement")
+
+   (list "{ x = 3; print x } " 3 "block statement")
+
+   (list "if zero?(0) { print 1 } { print 2 }" 1 "if statement")
+
+   (list "var x = 1, y = 2; {x = 3; y = 4; print +(x,y)}" 7 "example 1")
+
+   (list "
+   var x = 1, y = 2, z = 3; {
+     x = 3;
+     y = 4;
+     z = 0;
+     while not(zero?(x)) {
+       z = +(z,y); x = -(x,1)
+     };
+     print z
+   }
+      " 12 "example 2")
+
+   (list "
+   var x = 1; {
+     x = 3;
+     print x;
+     var x = 2;
+     {
+       x = 4;
+       print x
+     };
+     print x
+   }
+      " 3 "example 3")
+
+   (list "
+   var f = 1, x = 2; {
+     f = proc(x,y) *(x,y);
+     x = 3;
+     print (f 4 x)
+   }
+      " 12 "example 4")
+
+   (list "
+   var x = 0; {
+     read x;
+     print 1
+   }
+      " 1 "example 4")
+
+   (list "
+   var x = 1, y = 2, z = 3; {
+     x = 3;
+     y = 4;
+     z = 0;
+     do-while not(zero?(x)) {
+       z = +(z,y); x = -(x,1)
+     };
+     print z
+   }
+      " 12 "do while")
+   )
+  )
+
+(test-lang run sloppy->expval test-cases-statement)
