40ad4332f83f44b49a9f2da0db8b4666af74ab24
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-10 17:59:40 +0800

    feat: procedure support multiple arguments
    exer 4.32

--- ch4/4.5/exer-4.32/test.rkt
+++ ch4/4.5/exer-4.32/test.rkt
@@ -4,4 +4,22 @@
 (require "value.rkt")
 (require "../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-call-by-reference-lang)
+(test-lang run sloppy->expval
+           (append
+            test-cases-call-by-reference-lang
+            (list
+             (list "
+let swap = proc (x, y) let temp = x
+                  in begin
+                      set x = y;
+                      set y = temp
+                     end
+      in let a = 33
+         in let b = 44
+            in begin
+                (swap a b);
+                -(a,b)
+            end
+  " 11 "procedure support multiple arguments"))
+            )
+           )
