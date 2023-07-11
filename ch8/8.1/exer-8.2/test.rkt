d960c51218d4d30b9dfb5d40170672512a81e926
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-11 14:06:53 +0800

    feat: add only declared variables to module env
    definitions respect corresponding declaration when adding to env.
    add-module-defn-to-tenv doesn't suffer from the same problem.
    
    exer 8.2

--- ch8/8.1/exer-8.2/test.rkt
+++ ch8/8.1/exer-8.2/test.rkt
@@ -4,4 +4,31 @@
 (require "value.rkt")
 (require "../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-simple-modules)
+(define test-cases-add-declared-names-to-module-env-only
+  (list
+   (list "
+module m1
+  interface [
+    a : int
+    b : int
+    c : int
+  ]
+  body [
+    a = 33
+    x = -(a,1) %= 32
+    b = -(a,x) %= 1
+    c = -(x,b) %= 31
+  ]
+let a = 10
+      in from m1 take x
+      " 'error "Exmaple 8.1 internal x in m1 should not be visible")
+   )
+  )
+
+
+(test-lang run sloppy->expval
+           (append
+            test-cases-simple-modules
+            test-cases-add-declared-names-to-module-env-only
+            )
+           )
