f22ef3500ecae667e39e39f8324a443557661735
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-13 19:21:23 +0800

    feat: use unification algorithm when verifying module interface
    support optional type in proc-exp/letrec-exp, interface declaration
    must be specified explicitly.
    
    exer 8.10

--- ch8/8.1/exer-8.11/test.rkt
+++ ch8/8.1/exer-8.11/test.rkt
@@ -4,4 +4,27 @@
 (require "value.rkt")
 (require "../../../base/test.rkt")
 
-(test-lang run sloppy->expval test-cases-simple-modules)
+(define test-cases-type-inference
+  (list
+   (list "
+module m
+    interface [f : (int -> bool)]
+    body [f = proc (x : ?) x]
+1
+   " 'error "invalid proc type")
+
+   (list "
+module m
+    interface [f : (int -> int)]
+    body [f = proc (x : ?) x]
+1
+   " 1 "valid proc type")
+   )
+  )
+
+(test-lang run sloppy->expval
+           (append
+            test-cases-simple-modules
+            test-cases-type-inference
+            )
+           )
