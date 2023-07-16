94ef7930ef8fb7be292a60a75aff65318b4ebfd3
penghui <penghui.zhang@foxmail.com>
2023-07-16 20:50:33 +0800

    refactor: support multiple variables in single environment record
    update extend-env to extend-env*
    
    exer 8.16

--- ch8/8.2/exer-8.16/environment.rkt
+++ ch8/8.2/exer-8.16/environment.rkt
@@ -1,6 +1,6 @@
 #lang eopl
 
-(require racket/lazy-require "expression.rkt" "module.rkt")
+(require racket/list racket/lazy-require "expression.rkt" "module.rkt")
 (lazy-require
  ["value.rkt" (num-val expval? proc-val)]
  ["procedure.rkt" (procedure)]
@@ -9,9 +9,9 @@
 
 (define-datatype environment environment?
   (empty-env)
-  (extend-env
-   (var symbol?)
-   (val expval?)
+  (extend-env*
+   (vars (list-of symbol?))
+   (vals (list-of expval?))
    (saved-env environment?)
    )
   (extend-env-rec
@@ -29,12 +29,14 @@
 
 (define (apply-env env search-var)
   (cases environment env
-    (extend-env (var val saved-env)
-                (if (eqv? search-var var)
-                    val
-                    (apply-env saved-env search-var)
-                    )
-                )
+    (extend-env* (vars vals saved-env)
+                 (let ([index (index-of vars search-var)])
+                   (if index
+                       (list-ref vals index)
+                       (apply-env saved-env search-var)
+                       )
+                   )
+                 )
     (extend-env-rec (p-name b-var p-body saved-env)
                     (if (eqv? search-var p-name)
                         ; procedure env is extend-env-rec itself which contains procedure
@@ -60,7 +62,7 @@
 
 (define (lookup-module-name-in-env m-name env)
   (cases environment env
-    (extend-env (var val saved-env)
+    (extend-env* (vars vals saved-env)
                 (lookup-module-name-in-env m-name saved-env)
                 )
     (extend-env-rec (p-name b-var p-body saved-env)
