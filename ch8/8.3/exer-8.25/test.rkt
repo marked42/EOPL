ea4680f07cd0839aaea8c5863706f817037dbccc
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-20 19:35:26 +0800

    chore: start exer 8.25 on top of proc-modules

---
+++ ch8/8.3/exer-8.25/test.rkt
@@ -0,0 +1,13 @@
+#lang eopl
+
+(require "interpreter.rkt")
+(require "value.rkt")
+(require "../../../base/test.rkt")
+
+(test-lang run sloppy->expval
+           (append
+            test-cases-simple-modules
+            test-cases-opaque-types
+            test-cases-proc-modules
+            )
+           )
