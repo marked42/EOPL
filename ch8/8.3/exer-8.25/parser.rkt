d03919f8a0ce4dc107e3cc247a2c639ca28fbdb0
penghui <penghui.zhang@foxmail.com>
2023-07-20 22:53:28 +0800

    refactor: proc-module-body use typed-proc-module-param
    exer 8.25

--- ch8/8.3/exer-8.25/parser.rkt
+++ ch8/8.3/exer-8.25/parser.rkt
@@ -27,9 +27,11 @@
     (declaration ("transparent" identifier "=" type) transparent-type-declaration)
 
     (module-body ("[" (arbno definition) "]") definitions-module-body)
-    (module-body ("module-proc" "("identifier ":" interface")" module-body) proc-module-body)
+    (module-body ("module-proc" "("proc-module-param")" module-body) proc-module-body)
     (module-body (identifier) var-module-body)
     (module-body ("("identifier identifier")") app-module-body)
+
+    (proc-module-param (identifier ":" interface) typed-proc-module-param)
 
     (definition (identifier "=" expression) val-definition)
     (definition ("type" identifier "=" type) type-definition)
