a9aa46a12c1c9c6656ae5fdf6303a0a54d036238
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-20 19:35:26 +0800

    chore: start exer 8.25 on top of proc-modules

---
+++ ch8/8.3/exer-8.25/checker/renaming.rkt
@@ -0,0 +1,78 @@
+#lang eopl
+
+(require "../module.rkt" "type.rkt")
+
+(provide (all-defined-out))
+
+(define (rename-in-iface iface old new)
+    (cases interface iface
+        (simple-interface (decls)
+            (simple-interface (rename-in-decls decls old new))
+        )
+        (proc-interface (param-name param-iface result-iface)
+            (proc-interface
+                param-name
+                (rename-in-iface param-iface old new)
+                ; param-name shadows old in result-iface
+                (if (eqv? param-name old)
+                    result-iface
+                    (rename-in-iface result-iface old new)
+                )
+            )
+        )
+    )
+)
+
+; this isn't a map because we have let* scoping
+(define (rename-in-decls decls old new)
+    (if (null? decls)
+        '()
+        (cases declaration (car decls)
+            (var-declaration (var-name ty)
+                (cons
+                    (var-declaration var-name (rename-in-type ty old new))
+                    (rename-in-decls (cdr decls) old new)
+                )
+            )
+            (opaque-type-declaration (t-name)
+                (cons
+                    (opaque-type-declaration t-name)
+                    ; opaque type name shadows old, no need to replace in rest declarations
+                    (if (eqv? t-name old)
+                        (cdr decls)
+                        (rename-in-decls (cdr decls) old new)
+                    )
+                )
+            )
+            (transparent-type-declaration (t-name ty)
+                (cons
+                    (transparent-type-declaration t-name (rename-in-type ty old new))
+                    ; transparent type name shadows old, no need to replace in rest declarations
+                    (if (eqv? t-name old)
+                        (cdr decls)
+                        (rename-in-decls (cdr decls) old new)
+                    )
+                )
+            )
+        )
+    )
+)
+
+(define (rename-in-type ty old new)
+    (cases type ty
+        (proc-type (arg-type result-type)
+            (proc-type (rename-in-type arg-type old new) (rename-in-type result-type old new))
+        )
+        (named-type (name)
+            (named-type (rename-name name old new))
+        )
+        (qualified-type (m-name t-name)
+            (qualified-type (rename-name m-name old new) t-name)
+        )
+        (else ty)
+    )
+)
+
+(define (rename-name name old new)
+    (if (eqv? name old) new old)
+)
---
+++ ch8/8.3/exer-8.25/checker/type-environment.rkt
@@ -0,0 +1,107 @@
+#lang eopl
+
+(provide (all-defined-out))
+(require "type.rkt" "../module.rkt")
+
+(define-datatype type-environment type-environment?
+  (empty-tenv)
+  (extend-tenv
+   (var symbol?)
+   (type type?)
+   (saved-tenv type-environment?)
+   )
+  (extend-tenv-with-module
+   (name symbol?)
+   (interface interface?)
+   (saved-tenv type-environment?)
+   )
+  (extend-tenv-with-type
+   (name symbol?)
+   (ty type?)
+   (saved-tenv type-environment?)
+   )
+  )
+
+(define (apply-tenv env search-var)
+  (cases type-environment env
+    (extend-tenv (var type saved-tenv)
+                 (if (eqv? search-var var)
+                     type
+                     (apply-tenv saved-tenv search-var)
+                     )
+                 )
+    (else (eopl:error "Unbound variable ~s" search-var))
+    )
+  )
+
+(define (lookup-qualified-type-in-tenv m-name var-name tenv)
+  (let ([iface (lookup-module-name-in-tenv tenv m-name)])
+    (cases interface iface
+      (simple-interface (declarations)
+                        (lookup-variable-name-in-declarations var-name declarations)
+                        )
+      (proc-interface (param-name param-iface result-iface)
+                      (eopl:error 'lookup-qualified-type-in-tenv "proc-interface ~s" iface)
+                      )
+      )
+    )
+  )
+
+(define (lookup-module-name-in-tenv tenv m-name)
+  (cases type-environment tenv
+    (extend-tenv (var type saved-tenv)
+                 (lookup-module-name-in-tenv saved-tenv m-name)
+                 )
+    (extend-tenv-with-module (name iface saved-tenv)
+                             (if (eqv? name m-name)
+                                 iface
+                                 (lookup-module-name-in-tenv saved-tenv m-name)
+                                 )
+                             )
+    (extend-tenv-with-type (name ty saved-tenv)
+                           (lookup-module-name-in-tenv saved-tenv m-name)
+                           )
+    (empty-tenv () (eopl:error 'lookup-module-name-in-tenv "fail to find module ~s " m-name))
+    )
+  )
+
+(define (lookup-variable-name-in-declarations var-name declarations)
+  (if (null? declarations)
+      #f
+      (cases declaration (car declarations)
+        (var-declaration (this-var-name ty)
+                         (if (eqv? var-name this-var-name)
+                             ty
+                             (lookup-variable-name-in-declarations var-name (cdr declarations))
+                             )
+                         )
+        (opaque-type-declaration (this-var-name)
+                                 (eopl:error 'lookup-variable-name-in-declarations "can't take type of abstract type declaration ~s" (car declarations))
+                                 )
+        (transparent-type-declaration (this-var-name ty)
+                                      (if (eqv? var-name this-var-name)
+                                          ty
+                                          (lookup-variable-name-in-declarations var-name (cdr declarations))
+                                          )
+                                      )
+        )
+      )
+  )
+
+(define (lookup-type-name-in-tenv name tenv)
+  (cases type-environment tenv
+    (extend-tenv (var ty saved-tenv)
+                 (lookup-type-name-in-tenv name saved-tenv)
+                 )
+    (extend-tenv-with-module (m-name interface saved-tenv)
+                             (lookup-type-name-in-tenv name saved-tenv)
+                             )
+    (extend-tenv-with-type (t-name ty saved-tenv)
+                           (if (eqv? t-name name)
+                               ty
+                               (lookup-type-name-in-tenv name saved-tenv)
+                               )
+                           )
+    (empty-tenv () (eopl:error 'lookup-type-name-in-tenv "fail to find type ~s" name))
+    )
+  )
