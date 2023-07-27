2f83f23347de4c1418da944481a6a75e87597c11
penghui <penghui.zhang@foxmail.com>
2023-07-27 22:45:54 +0800

    feat: exer 9.5
    store host class name instead of super class name in method env

--- ch9/9.4/exer-9.5/method.rkt
+++ ch9/9.4/exer-9.5/method.rkt
@@ -3,7 +3,7 @@
 (require racket/lazy-require "parser.rkt" "expression.rkt")
 
 (lazy-require
- ["environment.rkt" (extend-env* extend-env-with-self-and-super empty-env)]
+ ["environment.rkt" (extend-env* extend-env-with-self-and-host empty-env)]
  ["store.rkt" (newref)]
  ["object.rkt" (object->fields lookup-class)]
  ["interpreter.rkt" (value-of-exp)]
@@ -15,17 +15,17 @@
   (a-method
    (vars (list-of symbol?))
    (body expression?)
-   (super-name symbol?)
+   (host-name symbol?)
    (field-names (list-of symbol?))
    )
   )
 
 (define (apply-method m self args)
   (cases method m
-    (a-method (vars body super-name field-names)
+    (a-method (vars body host-name field-names)
               (value-of-exp body
                         (extend-env* vars (map newref args)
-                                     (extend-env-with-self-and-super self super-name
+                                     (extend-env-with-self-and-host self host-name
                                                                      (extend-env* field-names (object->fields self)
                                                                                   (empty-env)
                                                                                   )
