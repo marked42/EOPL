016744341089068e9161c1184ef14893ac0b49d4
zhangpenghui04 <zhangpenghui04@meituan.com>
2023-07-28 17:09:30 +0800

    chore: start exer 9.9 on top of exer 9.8

---
+++ ch9/9.4/exer-9.9/test.rkt
@@ -0,0 +1,17 @@
+#lang eopl
+
+(require "interpreter.rkt")
+(require "value.rkt")
+(require "../../../base/test.rkt")
+
+(test-lang run sloppy->expval
+           (append
+            test-cases-implicit-refs-lang
+            test-cases-let-exp-with-multiple-declarations
+            test-cases-sum-exp
+            test-cases-list-v2
+            test-cases-proc-exp-with-multiple-arguments
+            test-cases-classes
+            test-cases-fieldref-fieldset
+            )
+           )
