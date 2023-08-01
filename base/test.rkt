4c7c758a4e44863910d501c61a1018f442bc3a4e
penghui <penghui.zhang@foxmail.com>
2023-08-02 00:44:24 +0800

    feat: final method
    exer 9.13

--- base/test.rkt
+++ base/test.rkt
@@ -3191,3 +3191,26 @@ let p = new colorpoint(3, 4, 77)
             " 'error "private field x/y cannot be accessed in subclass")
   )
 )
+
+(define test-cases-exer-final-method
+  (list
+   (list "
+class oddeven extends object
+  method initialize () 1
+  final method even (n)
+    if zero?(n)
+    then 1
+    else send self odd(-(n,1))
+  final method odd (n)
+    if zero?(n)
+    then 0
+    else send self even(-(n,1))
+
+class bogus-oddeven extends oddeven
+    method even()
+        0
+
+1
+            " 'error "final method cannot be overridden")
+  )
+)
